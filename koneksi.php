77b5a309dad3e394ce539e9808d4bc733195bbec
tegarpenemuanr3 <tegarpenemuan@gmail.com>
2021-04-02 22:15:07 +0700

    first commit

---
+++ koneksi.php
@@ -0,0 +1,15 @@
+<?php
+	$user  = 'root';
+	$pass = '';
+	try {
+		// buat koneksi dengan database
+		$koneksi = new PDO('mysql:host=localhost;dbname=tutorial_barang;',$user,$pass);
+		// set error mode
+		$koneksi->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	}catch (PDOException $e) {
+		// tampilkan pesan kesalahan jika koneksi gagal
+		print "Koneksi atau query bermasalah : " . $e->getMessage() . "<br/>";
+		die();
+	}
+	
+?>
---
+++ tutorial_barang.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 02 Apr 2021 pada 16.44
+-- Versi server: 10.1.36-MariaDB
+-- Versi PHP: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tutorial_barang`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_barang`
+--
+
+CREATE TABLE `tbl_barang` (
+  `id_barang` int(11) NOT NULL,
+  `nama_barang` varchar(255) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `harga_barang` int(11) NOT NULL,
+  `tgl_masuk` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_barang`
+--
+
+INSERT INTO `tbl_barang` (`id_barang`, `nama_barang`, `stok`, `harga_barang`, `tgl_masuk`) VALUES
+(1, 'Kipas Angin', 5, 500000, '2021-04-02'),
+(3, 'Laptop', 3, 5000000, '2021-04-02');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tbl_barang`
+--
+ALTER TABLE `tbl_barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_barang`
+--
+ALTER TABLE `tbl_barang`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
